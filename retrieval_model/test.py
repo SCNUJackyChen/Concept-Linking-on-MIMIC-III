6e9293070a469e9dbeae5ecfd872e457ef01eb7d
SCNUJackyChen <happyboyczq@qq.com>
2020-01-13 17:05:01 +0800

    add full code version

--- retrieval_model/test.py
+++ retrieval_model/test.py
@@ -3,6 +3,6 @@ import sys
 sys.path.append(os.path.abspath('.'))
 from retrieval_model.matching import MatchingModel
 
-model = MatchingModel('diag+desc-con50.csv', os.path.abspath('.') + '\\BioNLP-word-embedding.bin')
+model = MatchingModel('diag+desc-con50.csv', 'D:\科研\caml-mimic-master\word2vec\PubMed-shuffle-win-2.bin')
 cands = model.query(['infarction'], topk=25)
 print(cands)
